94b8be6d79b14d6c3332d81c5e3040ab2e2e4634
trung <tientrung14122012@gmail.com>
2022-12-22 23:42:35 +0700

    first commit

---
+++ Trung/assets/javascript/form.js
@@ -0,0 +1,10 @@
+
+var inputEmailQmk = document.getElementById('input__email--qmk')
+
+function quenmk(a) {
+    if (inputEmailQmk.value === '') {
+        alert('Chưa nhập email')
+    } else {
+        alert('Hệ thống sẽ gửi mật khẩu dự phòng vào email của bạn')
+    }
+}
